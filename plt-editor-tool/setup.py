532bd7bd6b867fd6220dcf6ef0aaf7bb5dc8c98d
Richard Couperthwaite <40215707+RichardCouperthwaite@users.noreply.github.com>
2020-02-15 20:39:44 -0600

    Update setup.py

---
+++ plt-editor-tool/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'plt-editor-tool',         # How you named your package folder (MyLib)
+  packages = ['plt-editor-tool'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='GNU GPLv3',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'GUI Pyplot Parameter Editor',   # Give a short description about your library
+  author = 'Richard Couperthwaite',                   # Type in your name
+  author_email = 'richard.couperthwaite@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/RichardCouperthwaite/plt-editor-tool',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['Matplotlib', 'Pyplot', 'GUI', 'Editor'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'tkcolorpicker',
+          'numpy',
+          'matplotlib',
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GNU GPLv3 License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
