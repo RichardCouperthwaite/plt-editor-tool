d48434adcf7f2c11c359a04ebb49d1b3b7f270d9
Richard Couperthwaite <40215707+RichardCouperthwaite@users.noreply.github.com>
2020-02-17 13:34:45 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 setup(
   name = 'pltEditorTool',         # How you named your package folder (MyLib)
   packages = ['pltEditorTool'],   # Chose the same as "name"
-  version = '1.0.11',    # Start with a small number and increase it with every change you make
+  version = '1.0.12',    # Start with a small number and increase it with every change you make
   license='GNU Lesser General Public License v3 (LGPLv3)',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'This module provide a GUI based approach for setting up and editing matplotlib plots',   # Give a short description about your library
   long_description='This module provide a GUI based approach for setting up and editing matplotlib plots',
   author = 'Richard Couperthwaite',                   # Type in your name
   author_email = 'richard.couperthwaite@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/RichardCouperthwaite/plt-editor-tool',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/RichardCouperthwaite/plt-editor-tool/archive/1.0.11.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/RichardCouperthwaite/plt-editor-tool/archive/1.0.12.tar.gz',    # I explain this later on
   keywords = ['Matplotlib', 'Pyplot', 'GUI', 'Editor'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'tkcolorpicker',
