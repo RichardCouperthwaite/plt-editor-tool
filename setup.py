5e9fa552f8ff1a0291c65ca2f6ed00d0f97d6cd3
Richard Couperthwaite <40215707+RichardCouperthwaite@users.noreply.github.com>
2020-02-21 10:55:55 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 setup(
   name = 'pltEditorTool',         # How you named your package folder (MyLib)
   packages = ['pltEditorTool'],   # Chose the same as "name"
-  version = '1.0.15',    # Start with a small number and increase it with every change you make
+  version = '1.1.0',    # Start with a small number and increase it with every change you make
   license='GNU Lesser General Public License v3 (LGPLv3)',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'This package provides a GUI based approach for setting up and editing matplotlib plots. Please see the Github repository for more detailed information on how to use the package',   # Give a short description about your library
-  long_description='This module provide a GUI based approach for setting up and editing matplotlib plots',
+  description = 'This package provides a GUI editing matplotlib plots',   # Give a short description about your library
+  long_description='This package provides a GUI based approach for setting up and editing matplotlib plots. Please see the Github repository for more detailed information on how to use the package',
   author = 'Richard Couperthwaite',                   # Type in your name
   author_email = 'richard.couperthwaite@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/RichardCouperthwaite/plt-editor-tool',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/RichardCouperthwaite/plt-editor-tool/archive/1.0.15.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/RichardCouperthwaite/plt-editor-tool/archive/1.1.0.tar.gz',    # I explain this later on
   keywords = ['Matplotlib', 'Pyplot', 'GUI', 'Editor'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'tkcolorpicker',
@@ -18,7 +18,7 @@ setup(
           'matplotlib',
       ],
   classifiers=[
-    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',   # Again, pick a license
