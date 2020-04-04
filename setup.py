1bfac2a2e5a975561fa20cef33e9c69a874911c9
Richard Couperthwaite <40215707+RichardCouperthwaite@users.noreply.github.com>
2020-04-04 12:07:27 -0500

    update
    Updated the usage guide

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.txt'), encoding='utf-8') as f:
 setup(
   name = 'pltEditorTool',
   packages = ['pltEditorTool'],
-  version = '2.0.5', 
+  version = '2.0.6', 
   license='GNU Lesser General Public License v3 (LGPLv3)',
   description = 'This package provides a GUI for editing matplotlib plots',   
   long_description=long_description,
@@ -17,7 +17,7 @@ setup(
   author = 'Richard Couperthwaite', 
   author_email = 'richard.couperthwaite@gmail.com',
   url = 'https://github.com/RichardCouperthwaite/plt-editor-tool',
-  download_url = 'https://github.com/RichardCouperthwaite/plt-editor-tool/archive/2.0.5.tar.gz',
+  download_url = 'https://github.com/RichardCouperthwaite/plt-editor-tool/archive/2.0.6.tar.gz',
   keywords = ['Matplotlib', 'Pyplot', 'GUI', 'Editor'], 
   install_requires=[
           'tkcolorpicker',
