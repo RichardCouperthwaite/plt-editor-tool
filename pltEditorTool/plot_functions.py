46bbbbed823e8519b84c52acd35495ae0f943396
Richard Couperthwaite <richardcouperthwaite@tamu.edu>
2020-07-30 09:23:05 -0500

    Updated Interface
    update of interface for QT

--- pltEditorTool/plot_functions.py
+++ pltEditorTool/plot_functions.py
@@ -20,7 +20,7 @@ except ImportError:
     
 
 def plot_fillbetween(ax, plot) :
-    ax.fill_between(np.array(plot['x']),
+    return ax.fill_between(np.array(plot['x']),
                     np.array(plot['y'])+np.array(plot['dif_top']),
                     np.array(plot['y'])-np.array(plot['dif_bot']),
                     alpha=plot['fill']['alpha'],
@@ -39,7 +39,7 @@ def plot_errorbar_x(ax, plot):
         marker_type = plot['marker']['type']
     else: 
         marker_type = 'None'
-    ax.errorbar(x=plot['x'], y=plot['y'],
+    return ax.errorbar(x=plot['x'], y=plot['y'],
                 yerr=plot['y_err'],
                 ecolor=plot['ebar']['color'],
                 elinewidth=plot['ebar']['linew'],
@@ -67,7 +67,7 @@ def plot_errorbar_y(ax, plot):
         marker_type = plot['marker']['type']
     else: 
         marker_type = 'None'
-    ax.errorbar(x=plot['x'], y=plot['y'],
+    return ax.errorbar(x=plot['x'], y=plot['y'],
                 xerr=plot['x_err'],
                 ecolor=plot['ebar']['color'],
                 elinewidth=plot['ebar']['linew'],
@@ -95,7 +95,7 @@ def plot_errorbar_xy(ax, plot):
         marker_type = plot['marker']['type']
     else: 
         marker_type = 'None'
-    ax.errorbar(x=plot['x'], y=plot['y'],
+    return ax.errorbar(x=plot['x'], y=plot['y'],
                 yerr=plot['y_err'], xerr=plot['x_err'],
                 ecolor=plot['ebar']['color'],
                 elinewidth=plot['ebar']['linew'],
@@ -123,7 +123,7 @@ def plot_plot(ax, plot):
         marker_type = plot['marker']['type']
     else: 
         marker_type = 'None'
-    ax.plot(plot['x'], plot['y'],
+    return ax.plot(plot['x'], plot['y'],
             color=plot['line']['color'],
             linestyle=linestyle,
             linewidth=plot['line']['width'],
@@ -286,8 +286,15 @@ class plot_class():
         ax = []
         colorbar = 0
         count = 0
+        
+        plots_for_legend = []
+        labels_for_legend = []
+        
+        axis_font_size = 0
+        
         for axis in self.axis_list:
             data = self.axis_data[axis]
+            axis_font_size = data['axis_text']['size']
             ax.append(self.fig.add_subplot(
                 self.gs[data['position'][0]:data['position'][0]+data['position'][2],
                         data['position'][1]:data['position'][1]+data['position'][3]]))
@@ -295,7 +302,11 @@ class plot_class():
                 plot = data['plots_data'][plot_num]
 
                 if plot['fill']['exist'] == 1 and len(plot['dif_top']) > 0:
-                    plot_fillbetween(ax[count], plot)
+                    if (plot['fill-label'] not in labels_for_legend) and (plot['fill-label'] != ''):
+                        plots_for_legend.append(plot_fillbetween(ax[count], plot))
+                        labels_for_legend.append(plot['fill-label'])
+                    else:
+                        plot_fillbetween(ax[count], plot)
                     label_length += 'label'
 
                 no_err_data = (plot['y_err'].size == 0 and plot['x_err'].size == 0)
@@ -307,14 +318,31 @@ class plot_class():
                 else:
                     if plot['ebar']['exist'] == 1 and not no_err_data:
                         if len(plot['y_err']) == 0:
-                            plot_errorbar_y(ax[count], plot)
+                            if (plot['label'] not in labels_for_legend) and (plot['label'] != ''):
+                                plots_for_legend.append(plot_errorbar_y(ax[count], plot))
+                                labels_for_legend.append(plot['label'])
+                            else:
+                                plot_errorbar_y(ax[count], plot)
                         if len(plot['x_err']) == 0:
-                            plot_errorbar_x(ax[count], plot)
+                            if (plot['label'] not in labels_for_legend) and (plot['label'] != ''):
+                                plots_for_legend.append(plot_errorbar_x(ax[count], plot))
+                                labels_for_legend.append(plot['label'])
+                            else:
+                                plot_errorbar_x(ax[count], plot)
                         if (len(plot['x_err']) != 0) and (len(plot['y_err']) != 0):
-                            plot_errorbar_xy(ax[count], plot)
+                            if (plot['label'] not in labels_for_legend) and (plot['label'] != ''):
+                                plots_for_legend.append(plot_errorbar_xy(ax[count], plot))
+                                labels_for_legend.append(plot['label'])
+                            else:
+                                plot_errorbar_xy(ax[count], plot)
                         label_length += 'label'
                     else:
-                        plot_plot(ax[count], plot)
+                        if (plot['label'] not in labels_for_legend) and (plot['label'] != ''):
+                            if (plot['label'] not in labels_for_legend) and (plot['label'] != ''):
+                                plots_for_legend.append(plot_plot(ax[count], plot))
+                                labels_for_legend.append(plot['label'])
+                            else:
+                                plot_plot(ax[count], plot)
                         label_length += 'label'
 
             plot_addlegend_labels(ax[count], data, label_length)
@@ -327,12 +355,26 @@ class plot_class():
                               fontweight=weights[self.axis_dict['Fig_title']['bold']],
                               fontsize=self.axis_dict['Fig_title']['size'],
                               wrap=True)
+        if self.axis_dict['Fig_legend']['position'] != 'None':
+            self.fig.legend(plots_for_legend, labels_for_legend,
+                            loc=self.axis_dict['Fig_legend']['position'], 
+                            ncol=self.axis_dict['Fig_legend']['ncol'],
+                            fontsize=self.axis_dict['Fig_legend']['size'])
+            if self.axis_dict['Fig_legend']['position'] == 'center right':
+                plt.subplots_adjust(right=self.axis_dict['Fig_legend']['correction'])
+                plt.tight_layout()
+            elif self.axis_dict['Fig_legend']['position'] == 'upper center':
+                plt.subplots_adjust(top=self.axis_dict['Fig_legend']['correction'],
+                                    bottom=axis_font_size/100-0.01, left=axis_font_size/100-0.05,wspace=(axis_font_size/100)*2+0.01, hspace=0.3)#axis_font_size/100-0.01
+            elif self.axis_dict['Fig_legend']['position'] == 'lower center':
+                plt.subplots_adjust(bottom=self.axis_dict['Fig_legend']['correction'],
+                                    left=axis_font_size/100-0.01,wspace=0.1, hspace=0.1)
         if len(cbar_map) > 0:
             for i in range(len(cbar_map)):
                 self.fig.colorbar(cbar_map[i], ax=cbar_axis[i])
         if save:
             self.fig.set_dpi(600)
-            self.fig.savefig(self.save_fname)
+            self.fig.savefig(self.save_fname, bbox_inches='tight')
             save_dir_list = self.save_fname.split('/')
             save_dir = ''
             for i in range(len(save_dir_list)-1):
--- pltEditorTool/pltEditorQT.py
+++ pltEditorTool/pltEditorQT.py
@@ -1079,48 +1079,48 @@ if __name__ == "__main__":
     plotEditor(x=x_data, y=y_data, x_err=x_err_data, y_err=y_err_data, 
                 fill=fill_data, fill_alt=fill_alt_data)
     
-    x_data = np.array(data.loc[:,'x.1'])
-    y_data = np.array(data.loc[:,'y.1'])
-    x_err_data = np.array(data.loc[:,'x_err.1'])
-    y_err_data = np.array(data.loc[:,'y_err.1'])
-    fill_data = np.array(data.loc[:,'fill.1'])
-    fill_alt_data = np.array(data.loc[:,'fill_alt.1'])
-    labels_data = 'Computation'
+    # x_data = np.array(data.loc[:,'x.1'])
+    # y_data = np.array(data.loc[:,'y.1'])
+    # x_err_data = np.array(data.loc[:,'x_err.1'])
+    # y_err_data = np.array(data.loc[:,'y_err.1'])
+    # fill_data = np.array(data.loc[:,'fill.1'])
+    # fill_alt_data = np.array(data.loc[:,'fill_alt.1'])
+    # labels_data = 'Computation'
     
-    print("Case 2: Single Numpy Array Input")
-    plotEditor(x=x_data, y=y_data, x_err=x_err_data, y_err=y_err_data, 
-                fill=fill_data, fill_alt=fill_alt_data)
+    # print("Case 2: Single Numpy Array Input")
+    # plotEditor(x=x_data, y=y_data, x_err=x_err_data, y_err=y_err_data, 
+    #             fill=fill_data, fill_alt=fill_alt_data)
     
-    x_data = np.array([np.array(data.loc[:,'x']), np.array(data.loc[:,'x.1'])]).transpose()
-    y_data = np.array([np.array(data.loc[:,'y']), np.array(data.loc[:,'y.1'])]).transpose()
-    x_err_data = np.array([np.array(data.loc[:,'x_err']), np.array(data.loc[:,'x_err.1'])]).transpose()
-    y_err_data = np.array([np.array(data.loc[:,'y_err']), np.array(data.loc[:,'y_err.1'])]).transpose()
-    fill_data = np.array([np.array(data.loc[:,'fill']), np.array(data.loc[:,'fill.1'])]).transpose()
-    fill_alt_data = np.array([np.array(data.loc[:,'fill_alt']), np.array(data.loc[:,'fill_alt.1'])]).transpose()
-    labels_data = ['Experimental', 'Computation']
+    # x_data = np.array([np.array(data.loc[:,'x']), np.array(data.loc[:,'x.1'])]).transpose()
+    # y_data = np.array([np.array(data.loc[:,'y']), np.array(data.loc[:,'y.1'])]).transpose()
+    # x_err_data = np.array([np.array(data.loc[:,'x_err']), np.array(data.loc[:,'x_err.1'])]).transpose()
+    # y_err_data = np.array([np.array(data.loc[:,'y_err']), np.array(data.loc[:,'y_err.1'])]).transpose()
+    # fill_data = np.array([np.array(data.loc[:,'fill']), np.array(data.loc[:,'fill.1'])]).transpose()
+    # fill_alt_data = np.array([np.array(data.loc[:,'fill_alt']), np.array(data.loc[:,'fill_alt.1'])]).transpose()
+    # labels_data = ['Experimental', 'Computation']
     
-    print("Case 3: 2D Numpy Array Input")
-    plotEditor(x=x_data, y=y_data, x_err=x_err_data, y_err=y_err_data, 
-                fill=fill_data, fill_alt=fill_alt_data)
+    # print("Case 3: 2D Numpy Array Input")
+    # plotEditor(x=x_data, y=y_data, x_err=x_err_data, y_err=y_err_data, 
+    #             fill=fill_data, fill_alt=fill_alt_data)
     
-    input_data = np.zeros((2,101,6))
-    input_data[0,:,0] = x_data[:,0]
-    input_data[1,:,0] = x_data[:,1]
-    input_data[0,:,1] = y_data[:,0]
-    input_data[1,:,1] = y_data[:,1]
-    input_data[0,:,2] = x_err_data[:,0]
-    input_data[1,:,2] = x_err_data[:,1]
-    input_data[0,:,3] = y_err_data[:,0]
-    input_data[1,:,3] = y_err_data[:,1]
-    input_data[0,:,4] = fill_data[:,0]
-    input_data[1,:,4] = fill_data[:,1]
-    input_data[0,:,5] = fill_alt_data[:,0]
-    input_data[1,:,5] = fill_alt_data[:,1]
-    print("Case 4: 3D Numpy Array Input")
-    plotEditor(x=input_data)
+    # input_data = np.zeros((2,101,6))
+    # input_data[0,:,0] = x_data[:,0]
+    # input_data[1,:,0] = x_data[:,1]
+    # input_data[0,:,1] = y_data[:,0]
+    # input_data[1,:,1] = y_data[:,1]
+    # input_data[0,:,2] = x_err_data[:,0]
+    # input_data[1,:,2] = x_err_data[:,1]
+    # input_data[0,:,3] = y_err_data[:,0]
+    # input_data[1,:,3] = y_err_data[:,1]
+    # input_data[0,:,4] = fill_data[:,0]
+    # input_data[1,:,4] = fill_data[:,1]
+    # input_data[0,:,5] = fill_alt_data[:,0]
+    # input_data[1,:,5] = fill_alt_data[:,1]
+    # print("Case 4: 3D Numpy Array Input")
+    # plotEditor(x=input_data)
     
-    print("Case 5: Pandas DataFrame Input")
-    plotEditor(x=data)
+    # print("Case 5: Pandas DataFrame Input")
+    # plotEditor(x=data)
 
 
 
